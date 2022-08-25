0a1ce6490d0a519efc2465817bee464515035d1a
Fock Space <a86gj3sp4@gmail.com>
2022-08-25 15:43:43 +0800

    break reviews and campgrounds to separate routers

--- schemas.js
+++ schemas.js
@@ -3,6 +3,7 @@ const Joi = require("joi");
 
 module.exports.campgroundSchema = Joi.object({
   campground: Joi.object({
+    location: Joi.string().required(),
     title: Joi.string().required(),
     price: Joi.number().required().min(0),
     image: Joi.string().required(),
