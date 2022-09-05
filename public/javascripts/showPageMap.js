19103f6700be672af182736c6b3cc9d80e09bf92
Fock Space <a86gj3sp4@gmail.com>
2022-09-05 10:26:53 +0800

    add map control

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -8,6 +8,9 @@ const map = new mapboxgl.Map({
   projection: "globe", // display the map as a 3D globe
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
+
 // Create a default Marker and add it to the map.
 const marker1 = new mapboxgl.Marker()
   .setPopup(
