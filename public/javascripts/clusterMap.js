c3f205736bbca347110d9bf7356bf67053fbb2ed
Fock Space <a86gj3sp4@gmail.com>
2022-09-06 15:53:37 +0800

    fix the center of clusteMap and title

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -3,7 +3,7 @@ const map = new mapboxgl.Map({
   container: "cluster-map",
   // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
   style: "mapbox://styles/mapbox/light-v10",
-  center: [-103.5917, 40.6699],
+  center: [120.9417,	24.23321],
   zoom: 6,
 });
 
